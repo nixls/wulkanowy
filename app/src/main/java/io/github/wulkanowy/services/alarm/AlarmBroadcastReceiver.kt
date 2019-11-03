f541f487ab0b6e7feba9ebf63f448082f3d6b7bc
Mikołaj Pich <m.pich@outlook.com>
2019-11-03 18:42:29 +0100

    Add *later* lesson in notif content text, add scheduling last lesson cancellation

--- app/src/main/java/io/github/wulkanowy/services/alarm/AlarmBroadcastReceiver.kt
+++ app/src/main/java/io/github/wulkanowy/services/alarm/AlarmBroadcastReceiver.kt
@@ -11,40 +11,51 @@ import io.github.wulkanowy.services.sync.channels.UpcomingLessonsChannel
 import io.github.wulkanowy.ui.modules.main.MainActivity
 import io.github.wulkanowy.ui.modules.main.MainView
 import io.github.wulkanowy.utils.getCompatColor
-import org.threeten.bp.Instant
-import org.threeten.bp.ZoneId
 import timber.log.Timber
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
 
     companion object {
-        const val LESSON_TITLE = "title"
-        const val LESSON_ROOM = "room"
-        const val LESSON_START = "start_timestamp"
-        const val LESSON_END = "end_timestamp"
         const val LESSON_TYPE = "type"
         const val NOTIFICATION_TYPE_CURRENT = 1
         const val NOTIFICATION_TYPE_UPCOMING = 2
+        const val NOTIFICATION_TYPE_LAST_LESSON_CANCELLATION = 3
+
+        const val LESSON_TITLE = "title"
+        const val LESSON_ROOM = "room"
+
+        const val LESSON_NEXT_TITLE = "next_title"
+        const val LESSON_NEXT_ROOM = "next_room"
+
+        const val LESSON_START = "start_timestamp"
+        const val LESSON_END = "end_timestamp"
     }
 
     override fun onReceive(context: Context, intent: Intent?) {
         val type = intent!!.getIntExtra(LESSON_TYPE, 0)
+
+        NotificationManagerCompat.from(context).cancelAll()
+        if (type == NOTIFICATION_TYPE_LAST_LESSON_CANCELLATION) return
+
         val subject = intent.getStringExtra(LESSON_TITLE)
-        val room = intent.getStringExtra(LESSON_ROOM)!!
+        val room = intent.getStringExtra(LESSON_ROOM)
+
+        val nextSubject = intent.getStringExtra(LESSON_NEXT_TITLE)
+        val nextRoom = intent.getStringExtra(LESSON_NEXT_ROOM)
         val start = intent.getLongExtra(LESSON_START, 0)
         val end = intent.getLongExtra(LESSON_END, 0)
         Timber.d("AlarmBroadcastReceiver receive intent: type: $type, subject: $subject, room: $room, start: $start")
 
-//        NotificationManagerCompat.from(context).cancel(type)
-        NotificationManagerCompat.from(context).cancelAll()
-        showNotification(context, type, subject, room, start, end)
+        showNotification(context, type, context.getString(if (type == NOTIFICATION_TYPE_CURRENT) R.string.timetable_now else R.string.timetable_next, "$subject ($room)"),
+            start, end, nextSubject?.let { context.getString(R.string.timetable_later, "$nextSubject ($nextRoom)") }
+        )
     }
 
-    private fun showNotification(context: Context, type: Int, subject: String?, room: String, start: Long, end: Long) {
+    private fun showNotification(context: Context, type: Int, title: String, start: Long, end: Long, next: String?) {
         NotificationManagerCompat.from(context).notify(type, NotificationCompat.Builder(context, UpcomingLessonsChannel.CHANNEL_ID)
-            .setContentTitle((if (type == NOTIFICATION_TYPE_CURRENT) context.getString(R.string.timetable_now) else context.getString(R.string.timetable_next)) + ": $subject ($room)")
+            .setContentTitle(title)
             .setWhen(if (type == NOTIFICATION_TYPE_CURRENT) end else start)
-            .setContentText("od ${start.toLocalDateTime()} do ${end.toLocalDateTime()}")
+            .setContentText(next)
             .setUsesChronometer(true)
             .setAutoCancel(false)
             .setOngoing(true)
@@ -55,6 +66,4 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
             .build()
         )
     }
-
-    private fun Long.toLocalDateTime() = Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDateTime()
 }
--- app/src/main/java/io/github/wulkanowy/services/alarm/AlarmHelper.kt
+++ app/src/main/java/io/github/wulkanowy/services/alarm/AlarmHelper.kt
@@ -8,11 +8,14 @@ import androidx.core.app.AlarmManagerCompat
 import io.github.wulkanowy.data.db.entities.Timetable
 import io.github.wulkanowy.data.repositories.preferences.PreferencesRepository
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_END
+import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_NEXT_ROOM
+import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_NEXT_TITLE
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_ROOM
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_START
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_TITLE
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.LESSON_TYPE
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.NOTIFICATION_TYPE_CURRENT
+import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.NOTIFICATION_TYPE_LAST_LESSON_CANCELLATION
 import io.github.wulkanowy.services.alarm.AlarmBroadcastReceiver.Companion.NOTIFICATION_TYPE_UPCOMING
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDateTime
@@ -45,29 +48,36 @@ class AlarmHelper @Inject constructor(
         if (!preferencesRepository.isUpcomingLessonsNotificationsEnable) return cancelNotifications(lessons, studentId)
 
         Timber.d("${lessons.size} to schedule, current millis: ${now().toTimestamp()}")
-        lessons.sortedBy { it.start }.forEachIndexed { index, lesson ->
-            val intent = with(Intent(context, AlarmBroadcastReceiver::class.java)) {
-                putExtra(LESSON_ROOM, lesson.room)
-                putExtra(LESSON_START, lesson.start.toTimestamp())
-                putExtra(LESSON_END, lesson.end.toTimestamp())
-                putExtra(LESSON_TITLE, lesson.subject)
-            }
+        lessons.groupBy { it.date }.map { week -> week.value.sortedBy { it.date } }.map { day ->
+            val numberOfLessons = day.size
+            day.forEachIndexed { index, lesson ->
+                val intent = with(Intent(context, AlarmBroadcastReceiver::class.java)) {
+                    putExtra(LESSON_ROOM, lesson.room)
+                    putExtra(LESSON_START, lesson.start.toTimestamp())
+                    putExtra(LESSON_END, lesson.end.toTimestamp())
+                    putExtra(LESSON_TITLE, lesson.subject)
+                    putExtra(LESSON_NEXT_TITLE, day.getOrNull(index + 1)?.subject)
+                    putExtra(LESSON_NEXT_ROOM, day.getOrNull(index + 1)?.room)
+                }
 
-            scheduleUpcoming(lessons, lesson, index, intent, studentId)
-            scheduleCurrent(lesson, intent, studentId)
+                scheduleUpcoming(lesson, day.getOrNull(index - 1), intent, studentId)
+                scheduleCurrent(lesson, intent, studentId)
+                if (numberOfLessons - 1 == index) scheduleLastLessonCancellation(lesson, intent, studentId)
+            }
         }
+
         Timber.d("Timetable notifications scheduled")
     }
 
-    private fun scheduleUpcoming(lessons: List<Timetable>, lesson: Timetable, index: Int, intent: Intent, studentId: Int) {
+    private fun scheduleUpcoming(lesson: Timetable, previous: Timetable?, intent: Intent, studentId: Int) {
         if (lesson.start >= now()) {
             Timber.d("${lesson.start} >= ${now()}")
-            val upcomingTimestamp = lessons.getOrNull(index - 1)?.end?.toTimestamp() ?: lesson.start.minusMinutes(30).toTimestamp()
+            val upcomingTimestamp = previous?.end?.toTimestamp() ?: lesson.start.minusMinutes(30).toTimestamp()
             AlarmManagerCompat.setExactAndAllowWhileIdle(alarmManager, AlarmManager.RTC_WAKEUP, upcomingTimestamp,
                 PendingIntent.getBroadcast(context, (upcomingTimestamp * studentId).toInt(), intent.also { it.putExtra(LESSON_TYPE, NOTIFICATION_TYPE_UPCOMING) }, PendingIntent.FLAG_CANCEL_CURRENT)
             )
-            Timber.d("- Scheduled lesson as upcoming at ${upcomingTimestamp.toLocalDateTime()} (${lessons.getOrNull(index - 1)?.end ?: lesson.start.minusMinutes(30)}) $lesson")
-        } else Timber.d("Not scheduled")
+            Timber.d("- scheduleUpcoming(): scheduled at ${upcomingTimestamp.toLocalDateTime()} (${previous?.end ?: lesson.start.minusMinutes(30)}) $lesson")
+        } else Timber.d("- scheduleUpcoming(): not scheduled")
     }
 
     private fun scheduleCurrent(lesson: Timetable, intent: Intent, studentId: Int) {
@@ -77,7 +87,18 @@ class AlarmHelper @Inject constructor(
             AlarmManagerCompat.setExactAndAllowWhileIdle(alarmManager, AlarmManager.RTC_WAKEUP, currentTimestamp,
                 PendingIntent.getBroadcast(context, (currentTimestamp * studentId).toInt(), intent.also { it.putExtra(LESSON_TYPE, NOTIFICATION_TYPE_CURRENT) }, PendingIntent.FLAG_CANCEL_CURRENT)
             )
-            Timber.d("- Scheduled lesson as current at ${currentTimestamp.toLocalDateTime()} (${lesson.start}) $lesson")
-        } else Timber.d("Not scheduled")
+            Timber.d("- scheduleCurrent(): scheduled at ${currentTimestamp.toLocalDateTime()} (${lesson.start}) $lesson")
+        } else Timber.d("- scheduleCurrent(): not scheduled")
+    }
+
+    private fun scheduleLastLessonCancellation(lesson: Timetable, intent: Intent, studentId: Int) {
+        if (lesson.end > now()) {
+            Timber.d("${lesson.end} > ${now()}")
+            val endTimestamp = lesson.end.toTimestamp()
+            AlarmManagerCompat.setExactAndAllowWhileIdle(alarmManager, AlarmManager.RTC_WAKEUP, endTimestamp,
+                PendingIntent.getBroadcast(context, (endTimestamp * studentId).toInt(), intent.also { it.putExtra(LESSON_TYPE, NOTIFICATION_TYPE_LAST_LESSON_CANCELLATION) }, PendingIntent.FLAG_CANCEL_CURRENT)
+            )
+            Timber.d("- scheduleLastLessonCancellation(): scheduled at ${endTimestamp.toLocalDateTime()} (${lesson.end}) $lesson")
+        } else Timber.d("- scheduleLastLessonCancellation(): not scheduled")
     }
 }
